a1e284a1583d8d3a66db641b03647c6980cd3fde
realshafer <realshafer@gmail.com>
2025-10-19 16:18:38 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8;
+let sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9, sound10, sound11, sound12;
 let original1;
 let questionButton, original1Button, answerButton, nextButton;
 let player, fileName;
@@ -6,15 +6,20 @@ let answerRevealed = false;
 
 function preload() {
   // Question pool
-  sound1 = loadSound('assets/104.mp3');
-  sound2 = loadSound('assets/112.mp3');
-  sound3 = loadSound('assets/120.mp3');
-  sound4 = loadSound('assets/128.mp3');
-  sound5 = loadSound('assets/136.mp3');
-  sound6 = loadSound('assets/144.mp3');
-  sound7 = loadSound('assets/152.mp3');
+  sound1 = loadSound('assets/Bb4 Sin.mp3');
+  sound2 = loadSound('assets/Bb4 Tri.mp3');
+  sound3 = loadSound('assets/Bb4 Saw.mp3');
+  sound4 = loadSound('assets/Bb4 Sq.mp3');
+  sound5 = loadSound('assets/C3 Sin.mp3');
+  sound6 = loadSound('assets/C3 Tri.mp3');
+  sound7 = loadSound('assets/C3 Saw.mp3');
+  sound8 = loadSound('assets/C3 Sq.mp3');
+  sound9 = loadSound('assets/D1 Sin.mp3');
+  sound10 = loadSound('assets/D1 Tri.mp3');
+  sound11 = loadSound('assets/D1 Saw.mp3');
+  sound12 = loadSound('assets/D1 Sq.mp3');
   // Originals
-  original1 = loadSound('assets/120-click.mp3');
+  // original1 = loadSound('assets/120-click.mp3');
 }
 
 function setup(){  
@@ -25,11 +30,11 @@ function setup(){
 
   // Title
   textSize(36);
-  text("Tempo ID Practice", width/2, height/9);
+  text("Basic Waveform Practice", width/2, height/9);
 
   // Subtitle
   textSize(20);
-  text("BPM = 104, 112, 120, 128, 136, 144, 152", width/2, height/9 + 40);
+  text("Sine, Triangle, Sawtooth, Square", width/2, height/9 + 40);
 
   // --- Layout variables ---
   let rowH = 60;
@@ -45,25 +50,17 @@ function setup(){
   styleButton(questionButton, col2X, startY, "#00E938");
   questionButton.mousePressed(toggleQuestion);
 
-  // ORIGINAL 1 row
-  createDiv("BPM = 120")
-    .position(col1X - 150, startY + rowH)
-    .style("color","white").style("font-size","24px");
-  original1Button = createButton("PLAY");
-  styleButton(original1Button, col2X, startY + rowH, "#00E938");
-  original1Button.mousePressed(toggleOriginal1);
-
   // ANSWER row
   createDiv("ANSWER")
-    .position(col1X - 150, startY + rowH*2)
+    .position(col1X - 150, startY + rowH)
     .style("color","white").style("font-size","24px");
   answerButton = createButton("REVEAL");
-  styleButton(answerButton, col2X, startY + rowH*2, "#03A9F4");
+  styleButton(answerButton, col2X, startY + rowH, "#03A9F4");
   answerButton.mousePressed(showAnswer);
 
   // NEXT QUESTION button
   nextButton = createButton("NEXT QUESTION");
-  nextButton.position(width/2 - 100, startY + rowH*3 + 20);
+  nextButton.position(width/2 - 100, startY + rowH*2 + 20);
   nextButton.size(200, rowH);
   nextButton.style("font-size","20px");
   nextButton.style("background-color","#FFC107");
@@ -92,18 +89,6 @@ function toggleQuestion() {
     player.amp(0.8);
     player.loop();
     questionButton.html("STOP").style("background-color","#F80F05");
-  }
-}
-
-function toggleOriginal1() {
-  if (original1.isPlaying()) {
-    original1.stop();
-    resetButton(original1Button, "PLAY", "#00E938");
-  } else {
-    stopAll();
-    original1.amp(0.8);
-    original1.loop();
-    original1Button.html("STOP").style("background-color","#F80F05");
   }
 }
 
@@ -138,17 +123,22 @@ let secondLastChoice = -1;
 function chooseSound() {
   let choice;
   do {
-    choice = int(random(7));
+    choice = int(random(12));
   } while (choice === lastChoice && choice === secondLastChoice);
 
   secondLastChoice = lastChoice;
   lastChoice = choice;
 
-  if (choice === 0) { player = sound1; fileName = "BPM = 104"; }
-  else if (choice === 1) { player = sound2; fileName = "BPM = 112"; }
-  else if (choice === 2) { player = sound3; fileName = "BPM = 120"; }
-  else if (choice === 3) { player = sound4; fileName = "BPM = 128"; }
-  else if (choice === 4) { player = sound5; fileName = "BPM = 136"; }
-  else if (choice === 5) { player = sound6; fileName = "BPM = 144"; }
-  else { player = sound7; fileName = "BPM = 152"; }
+  if (choice === 0) { player = sound1; fileName = "Sine"; }
+  else if (choice === 1) { player = sound2; fileName = "Triangle"; }
+  else if (choice === 2) { player = sound3; fileName = "Sawtooth"; }
+  else if (choice === 3) { player = sound4; fileName = "Square"; }
+  else if (choice === 4) { player = sound5; fileName = "Sine"; }
+  else if (choice === 5) { player = sound6; fileName = "Triangle"; }
+  else if (choice === 6) { player = sound7; fileName = "Sawtooth"; }
+  else if (choice === 7) { player = sound8; fileName = "Square"; }
+  else if (choice === 8) { player = sound9; fileName = "Sine"; }
+  else if (choice === 9) { player = sound10; fileName = "Triangle"; }
+  else if (choice === 10) { player = sound11; fileName = "Sawtooth"; }
+  else { player = sound12; fileName = "Square"; }
 }
